571744a9fb30db1a24e476cd72df3fbd5361d915
Sek Sitha <seksitha@gmail.com>
2023-01-18 13:29:14 +0700

    add ramtron

--- libraries/AP_HAL_ChibiOS/hwdef/scripts/STM32F407xx.py
+++ libraries/AP_HAL_ChibiOS/hwdef/scripts/STM32F407xx.py
@@ -12,12 +12,15 @@ build = {
 
 # MCU parameters
 mcu = {
+    # location of MCU serial number
+    'UDID_START' : 0x1FFF7A10,
+    
     # ram map, as list of (address, size-kb, flags)
     # flags of 1 means DMA-capable
     # flags of 2 means faster memory for CPU intensive work
     'RAM_MAP' : [
         (0x20000000, 128, 1), # main memory, DMA safe
-        (0x10000000,  64, 2), # CCM memory, faster, but not DMA safe
+        (0x20020000, 128, 2), # CCM memory, faster, but not DMA safe
     ],
 
 	'EXPECTED_CLOCK' : 168000000,
